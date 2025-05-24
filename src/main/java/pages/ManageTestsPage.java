8dc24ac6ea62f09f917b9cfdeb24ceb4d17e74fc
arnab-3797 <arnabsamanta046@gmail.com>
2025-05-24 11:03:27 +0530

    Updating ManageTetPage

--- src/main/java/pages/ManageTestsPage.java
+++ src/main/java/pages/ManageTestsPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -20,6 +22,26 @@ public class ManageTestsPage extends BasePage {
 		
 		addtestbtn.click();
 		return new AddTestPage(driver);
+	}
+	
+	
+	@FindBy(xpath="//input[@placeholder='Search']")
+	public static WebElement searchfield;
+	@FindBy(xpath="//table[@class='MuiTable-root']//tr")
+	public static List<WebElement> searchresults;
+	
+	public void searchFunctionality(String searchtext) {
+		
+		searchfield.clear();
+		searchfield.sendKeys(searchtext);
+		
+		for(WebElement result : searchresults){
+			
+			if(result.getText().toLowerCase().contains(searchtext.toLowerCase())) {
+				result.click();
+			}
+		}
+		
 	}
 
 }
